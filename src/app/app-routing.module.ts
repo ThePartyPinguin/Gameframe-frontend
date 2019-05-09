ed851e2db8b00ec07408e844cfcdf53de6158004
Joris Eijmberts <j.eijmberts@student.fontys.nl>
2019-05-09 17:56:00 +0200

    Added auth guard to header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import {LandingComponent} from './components/landing/landing.component';
 import {LoginComponent} from './components/auth/login/login.component';
 import {RegisterComponent} from './components/auth/register/register.component';
 import {UserProfileComponent} from './components/user/user-profile/user-profile.component';
+import {AuthGuard} from './guards/authentication.guards';
+import {LogoutComponent} from './components/auth/logout/logout.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent},
-  {path: 'timeline', component: LandingComponent},
+  {path: 'timeline', component: LandingComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: UserProfileComponent},
+  {path: 'logout', component: LogoutComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: UserProfileComponent, canActivate: [AuthGuard]},
   {path: 'profile/:username', component: UserProfileComponent}
 ];
 
